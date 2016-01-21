d37832a924e84f0cea265ddcf13288476f804a4e
gborja <g.borja@klab.com>
2016-01-21 12:15:25 +0800

    Update testIP.php

--- testIP.php
+++ testIP.php
@@ -3,6 +3,7 @@ echo $_SERVER['SERVER_ADDR'];
 echo "\nlb2.betarealm.com";
 echo "\ncheck CaPISTRANO";
 echo "\nweeeeeeeeeeeeeeeeeeeeeeeeeeewwwwwwwwwwww";
+echo "\nweeeeeeeeeeeeeeeeeeeeeeeeeeewwwwwwwwwwww";
 #$ip = gethostbyname('lb2.betarealm.com');
 #echo $ip;
 ?>
